283da24bd7a8b29265a08cb33aac1fba7d6eb4b1
xhhs530 <2323186582@qq.com>
2019-12-18 20:31:18 +0800

    引入element

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <el-button type='primary' plain>默认按钮</el-button>
+    <el-button type='primary'>默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
