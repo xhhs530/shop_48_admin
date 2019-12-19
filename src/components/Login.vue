de357c4f238aa778319666cb326c6beb8635cd23
xhhs530 <2323186582@qq.com>
2019-12-19 19:33:01 +0800

    修改Loginn

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
-    <el-button type='primary' plain>默认按钮</el-button>
-    <el-button type='primary'>默认按钮</el-button>
+    <div>这是登录组件</div>
+    <div>这是登录组件的文本</div>
   </div>
 </template>
 
@@ -12,4 +12,5 @@ export default {
 </script>
 
 <style>
+
 </style>
