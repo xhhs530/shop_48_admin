4f3a6bc965f9ceedd584cf6844d60494e674309b
xhhs530 <2323186582@qq.com>
2019-12-21 18:48:38 +0800

    login功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-
+import Index from '../components/Index.vue'
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    component: Login
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/',
+      redirect: Index
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: Index
+    }
+  ]
 })
-
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (token || to.pash === '/login') {
+    next()
+  } else {
+    next('/login')
+  }
+})
 export default router
