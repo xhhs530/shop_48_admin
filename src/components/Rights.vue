67c7c514b0f0b8fdfb126058d326f87093a578a0
xhhs530 <2323186582@qq.com>
2020-02-11 16:34:28 +0800

    权限列表完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,12 +1,53 @@
 <template>
   <div class="rights">
-    这是权限组件
+<el-breadcrumb separator-class="el-icon-arrow-right">
+  <el-breadcrumb-item :to="{ path: '/index' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+  <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+</el-breadcrumb>
+ <el-table
+      :data="rightList"
+    >
+      <el-table-column
+        prop="authName"
+        label="权限名称"
+       >
+      </el-table-column>
+            <el-table-column
+        prop="path"
+        label="路径"
+       >
+      </el-table-column>
+            <el-table-column
+        prop="date"
+        label="层级"
+       >
+       <template v-slot:default="{row}">
+         <p v-if="row.level==='0'">一级</p>
+         <p v-if="row.level==='1'">二级</p>
+         <p v-if="row.level==='2'">三级</p>
+       </template>
+      </el-table-column>
+ </el-table>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      rightList: []
+    }
+  },
+  async created () {
+    const { meta, data } = await this.$axios.get('rights/list')
+    if (meta.status === 200) {
+      this.rightList = data
+      console.log(data)
+    } else {
+      this.$message.error(meta.msg)
+    }
+  }
 }
 </script>
 
